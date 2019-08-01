cb131bfa0f8de614b8c92c4a91af2a78441fc2db
Louis Tannudin <ltannudin@gmail.com>
2019-08-01 11:58:09 -0400

    Created Security Account type

--- BankATM/Account.java
+++ BankATM/Account.java
@@ -1,18 +1,25 @@
 public class Account {
     protected int accountNumber;
-    protected int type; // 0: Default, 1: Checking, 2:Saving
-    protected Currency[] c = new Currency[3];
+    protected int type; // 0: Default, 1: Checking, 2:Saving, 3: Security
+    protected Currency[] c;
 
 
     public Account(int type, int accountNumber) {
         this.type = type;
         this.accountNumber = accountNumber;
-        c[0] = new Currency("USD", '$');
-        c[1] = new Currency("RMB", '¥');
-        c[2] = new Currency("EUR", '€');
+        if(type < 3){
+            c = new Currency[3];
+            c[0] = new Currency("USD", '$');
+            c[1] = new Currency("RMB", '¥');
+            c[2] = new Currency("EUR", '€');
+        }else if(type == 3){
+            this.c = new Currency[1];
+            c[0] = new Currency("USD", '$');
+        }
+
     }
 
-    public Account(int type) {  
+    public Account(int type) {
         this(type, -1);
     }
 
@@ -37,15 +44,27 @@ public class Account {
         else if(this.getType()==2){
             info += "Saving";
         }
+        else if(this.getType() == 3){
+            info += "Security";
+        }
         info += "  Account\n";
-        for(int i=0;i<=2;i++){
-            if(c[i].getBalance()==0){
-                continue;
+        if(this.getType() <3) {
+            for (int i = 0; i <= 2; i++) {
+                if (c[i].getBalance() == 0) {
+                    continue;
+                }
+                info += c[i].getName();
+                info += " ";
+                info += c[i].getSymbol();
+                info += c[i].getBalance();
+                info += " ";
             }
-            info += c[i].getName();
+        }
+        else if(this.getType() == 3){
+            info += c[0].getName();
             info += " ";
-            info += c[i].getSymbol();
-            info += c[i].getBalance();
+            info += c[0].getSymbol();
+            info += c[0].getBalance();
             info += " ";
         }
         info += "\n";
