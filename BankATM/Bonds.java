3a90b0c794bf99ef9725b2d40f24ccd58f116c08
Louis Tannudin <ltannudin@gmail.com>
2019-08-03 14:56:13 -0400

    Changes made

--- BankATM/Bonds.java
+++ BankATM/Bonds.java
@@ -3,14 +3,12 @@ public class Bonds {
     private double interest;
     private int maturity; //how long the bond is until receive interest.
     private String typeOfBond; //week or Month 
-    private int bondId; //to differeniate between similar bonds
-    
+
     public Bonds(int value, double i, int length, String type, int id) {
       amount = value;
       interest = i;
       maturity = length;
       typeOfBond = type; 
-      bondId = id;
     }
     public int getAmount() { //return bond's value
       return amount;
@@ -26,9 +24,6 @@ public class Bonds {
     }
     public String getTypeofBond() { //return whether a month or week bond
       return typeOfBond;
-    }
-    public int getBondId() { //return bond's id
-      return bondId;
     }
     public double getMaturityValue(){// returns bond's value at maturity
       double x = interest + 1;
---
+++ BankATM/customerBond.java
@@ -0,0 +1,8 @@
+public class customerBond {
+    private int amount;
+    private double interest;
+    private int maturity; //how long the bond is until receive interest.
+    private String typeOfBond; //week or Month
+    private
+
+}
