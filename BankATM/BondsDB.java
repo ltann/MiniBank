9ec0738bcd136c7a4d4a9b59c0b5a5fffc30aace
hqq777 <48264564+hqq777@users.noreply.github.com>
2019-08-02 23:36:38 -0400

    Add files via upload

---
+++ BankATM/BondsDB.java
@@ -0,0 +1,59 @@
+package bank;
+
+public class BondsDB {
+	private String bondID;
+	private int amount;
+	private double interest;
+	private int maturity;
+	private String bondType;
+	
+	public BondsDB(String bondID, int amount, double interest, int maturity, String bondType) {
+		this.bondID = bondID;
+		this.amount = amount;
+		this.interest = interest;
+		this.maturity = maturity;
+		this.bondType = bondType;
+	}
+
+	public String getBondID() {
+		return bondID;
+	}
+
+	public void setBondID(String bondID) {
+		this.bondID = bondID;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public double getInterest() {
+		return interest;
+	}
+
+	public void setInterest(double interest) {
+		this.interest = interest;
+	}
+
+	public int getMaturity() {
+		return maturity;
+	}
+
+	public void setMaturity(int maturity) {
+		this.maturity = maturity;
+	}
+
+	public String getBondType() {
+		return bondType;
+	}
+
+	public void setBondType(String bondType) {
+		this.bondType = bondType;
+	}
+	
+	
+}
