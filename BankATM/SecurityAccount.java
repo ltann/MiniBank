338d0a985650aaa9c115515f6a53c3ce8e036c2b
Louis Tannudin <ltannudin@gmail.com>
2019-08-03 23:05:42 -0400

    Added Stocks constructor

--- BankATM/SecurityAccount.java
+++ BankATM/SecurityAccount.java
@@ -1,103 +1,85 @@
 import java.util.*;
 
 public class SecurityAccount extends Account {
-    private Currency usd; //USD
     private LinkedList<customerStock> stocks;
     private LinkedList<customerBond> bonds;
-    private double availableFunds ;
+    private double availableFunds;
     private double valueOfSA;
+    private LinkedList<String> Transactions;
+    private double profitMade;
 
-    public SecurityAccount(int type, int accountNumber, int funds){
+    public SecurityAccount(int type, int accountNumber, int funds) {
         super(type, accountNumber);
-        this.usd = this.getC()[0];
-        this.stocks = new LinkedList<>();
-        this.bonds = new LinkedList<>();
+        super.c = this.getC();
+        this.stocks = new LinkedList<customerStock>();
+        this.bonds = new LinkedList<customerBond>();
         availableFunds = funds;
         valueOfSA = funds;
     }
+
     public double getAvailableFunds() {
-      return availableFunds;
-    }
-
-    public void updateValueOfSA(){
-        double value = usd.getBalance();
-        ListIterator<customerStock> stockListIterator = stocks.listIterator();
-        while(stockListIterator.hasNext()){
-            customerStock st = stockListIterator.next();
-            value += st.getPriceBoughtAt() * st.getNumShares();
-        }
-
-        ListIterator<customerBond> bondsListIterator = bonds.listIterator();
-        while(bondsListIterator.hasNext()){
-            customerBond bd = bondsListIterator.next();
-            value += bd.getAmount();
-        }
-
+        return availableFunds;
     }
 
     public double getValueOfSA() {
-      return valueOfSA;
+        return valueOfSA;
     }
 
-    public void purchaseStock(String ticker, String StockName, double priceOfShare, double numOfShares) {
-      //change available Balance
-      double Sharevalue = priceOfShare*numOfShares;
-      double canPurchase = availableFunds -  Sharevalue;
-      if(canPurchase < 0) { //not enough funds --> should print error
-        
-      }else {
-        availableFunds  = availableFunds -  Sharevalue;
-      //Change hash of Stock
-//       Stocks stock = stocks.get(ticker);
-      }
-    }
-    public void sellStock(String ticker, String StockName, double priceOfShare, double numOfShares) {
-      //change available Balance and valueOfSA
-      double Sharevalue = priceOfShare*numOfShares;
-      availableFunds  = availableFunds -  Sharevalue;
-    }
-
-    private void containsStocks(){
-        ListIterator<customerStock> i = stocks.listIterator();
-
-        while(i.hasNext()){
-            System.out.println(i.next().getTicker());
+    public boolean purchaseStock(Stocks stock, int numOfShares) {
+        boolean purchasable = true;
+        double shareValue = stock.getPricePerShare() * numOfShares;
+        if (shareValue > availableFunds) {
+            System.out.println("You're available funds are: " + availableFunds + " you cannot purchase this stock share value at: " + shareValue);
+            purchasable = false;
+        } else {
+            availableFunds = availableFunds - shareValue;
+            valueOfSA = availableFunds;
+            stocks.add(new customerStock(stock.getTicker(), stock.getStockName(), stock.getPricePerShare(), numOfShares));
+            //UPDATE DAILY? TRANSACTIONS
+            Transactions.add("Purchased " + numOfShares + " of " + stock.getStockName() + " Share(s) at " + stock.getPricePerShare());
+            profitMade -= shareValue;
         }
+        return purchasable;
     }
-    
+
+    public boolean sellStock(customerStock customerStock, double currentStockPrice, int numOfShares) {
+        //change available Balance and valueOfSA
+        boolean sellable = true;
+        if(customerStock.getNumShares() < numOfShares){
+            sellable = false;
+            System.out.println("You only have " + customerStock.getNumShares() + " shares, so you can't sell " + numOfShares + " of those...");
+        }
+        else{
+            double shareValue = currentStockPrice * numOfShares;
+            double profits = shareValue - (customerStock.getNumShares() * customerStock.getPriceBoughtAt());
+            availableFunds += shareValue;
+            valueOfSA += profits;
+            stocks.remove(customerStock);
+            Transactions.add("Sold " + numOfShares + " of " + customerStock.getStockName() + " Share(s) at " + currentStockPrice);
+            profitMade += shareValue;
+        }
+        return sellable;
+    }
 
 //    public void addBonds(Bonds b){
 //        bonds.put(b);
 //    }
-//
 //    public Bonds getBonds() {
-//
-//    }
-//
-
-//    public Stocks getStocks(){//get stocks
-//
-//        return stock;
 //    }
 
-    public Currency getUsd() {
-        return usd;
+    public LinkedList<customerStock> getStocks(){//get all Stocks
+        return this.stocks;
     }
 
-    public double getBalance(){
-        return this.usd.getBalance();
+    public LinkedList<customerBond> getBonds() {
+        return bonds;
     }
 
-    public String portfolio(){
-        StringBuilder str = new StringBuilder();
-        return str.toString();
+    public double getBalance() {
+        return super.c[0].getBalance();
     }
 
-    public static void main(String[] args){
-        SecurityAccount i = new SecurityAccount(3, 33, 3);
-        i.stocks.add(new customerStock("Apple", "a", 5.0,3));
-        i.stocks.add(new customerStock("asdf", "a", 5.0,2));
-        i.containsStocks();
-
+    public LinkedList<String> getTransactions() {
+        return Transactions;
     }
 }
--- BankATM/customerStock.java
+++ BankATM/customerStock.java
@@ -1,11 +1,13 @@
 public class customerStock extends Stocks{
     private double priceBoughtAt;
     private int numShares;
+    private double unrealizedProfitOrLoss; //positive if a gain, negative if a loss but 0 if neither.
 
     public customerStock(String ticker, String stockName, double pricePerShare, int numShares){
         super(ticker, stockName, pricePerShare);
         this.priceBoughtAt = pricePerShare;
         this.numShares = numShares;
+        this.unrealizedProfitOrLoss = 0;
     }
 
     public int getNumShares() {
@@ -14,5 +16,19 @@ public class customerStock extends Stocks{
 
     public double getPriceBoughtAt() {
         return priceBoughtAt;
+    }
+
+    public double getUnrealizedProfitOrLoss(Stocks stocks) {
+        double unrealized = stocks.getPricePerShare() - this.getPriceBoughtAt();
+        if(unrealized < 0){
+            System.out.println("There will be a loss in profit of " + unrealized + " USD.");
+        }
+        else if(unrealized > 0){
+            System.out.println("There will be a gain in profit of " + unrealized + " USD.");
+        }
+        else{
+            System.out.println("There will be no loss or profit from selling at this price");
+        }
+        return unrealized;
     }
 }
