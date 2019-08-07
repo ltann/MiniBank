25092f24ce52421e993fdb28d1fd08f4b1698cf0
hqq777 <hqqqqqq1002@gmail.com>
2019-08-07 00:17:54 -0400

    Create LoanDB.java

---
+++ BankATM/LoanDB.java
@@ -0,0 +1,18 @@
+public class LoanDB extends Loan {
+	
+	private String userName;
+
+	public LoanDB(int loanID, double interest, double amount, String type, int boughtAt, String userName) {
+		super(loanID, interest, amount, type, boughtAt);
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+}
